fd7c55484ef52bc9da1c6989439e0c0c5e4562de
Agame7k <comaidenaiden@gmail.com>
2024-05-21 21:11:01 -0500

    Update arduino-cli path in upload_arduino.py

--- upload_arduino.py
+++ upload_arduino.py
@@ -3,7 +3,7 @@ import subprocess
 import sys
 
 # Define paths and filenames
-ino_file = "/home/jarvis/JARVIS/JarArdu.ino"  # Corrected path to the .ino file
+ino_file = "/home/jarvis/JARVIS/JARVIS.ino"  # Corrected path to the .ino file
 arduino_cli = "/home/jarvis/bin/arduino-cli"  # Correct path to arduino-cli
 fqbn = "arduino:avr:uno"
 port = "/dev/ttyUSB0"  # Adjust according to your setup, typically /dev/ttyUSB0 or /dev/ttyACM0
